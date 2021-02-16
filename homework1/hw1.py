14a36a5514e566afc5d2e835fa9baec820ae8ce7
dunyahasan <78453394+dunyahasan@users.noreply.github.com>
2021-02-15 23:32:47 -0500

    Update hw1.py

--- homework1/hw1.py
+++ homework1/hw1.py
@@ -11,7 +11,7 @@ def return_number_3():
 def return_string_vcu():
     """ This function should return a string with the lowercase value of vcu"""
 
-    return_value = vcu.lower()
+    return_value = "vcu".lower()
     return return_value
 
 
