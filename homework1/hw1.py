a30d5fca8ff5d97461cf3d64f42fd92477d228da
dunyahasan <78453394+dunyahasan@users.noreply.github.com>
2021-02-15 23:44:18 -0500

    Update hw1.py

--- homework1/hw1.py
+++ homework1/hw1.py
@@ -3,14 +3,14 @@
 def return_number_3():
     """ This function should return an integer with the value of 3"""
 
-    return_value = 3
+    return_value = 3 
     return return_value
 
 
 def return_string_vcu():
     """ This function should return a string with the lowercase value of vcu"""
 
-    return_value = "vcu".lower() 
+    return_value = "vcu".lower()  
     return return_value
 
 
@@ -18,7 +18,7 @@ def return_lowercased_string(input_string):
     """You have a variable called input_string that is of type string.
     Return it but the lowercase version of it."""
 
-    return_value = input_string.lower() 
+    return_value = input_string.lower()  
     return return_value
 
 
@@ -26,12 +26,12 @@ def return_without_starting_ending_whitespace(input_string):
     """You have a variable called input_string that is of type string.
     Return it but with the surrounding (left and right) whitespace stripped."""
 
-    return_value = input_string.strip() 
+    return_value = input_string.strip()  
     return return_value
 
 
 def return_addition(first_number, second_number):
     """ Return the two numbers added together. """
 
-    return_value = first_number + second_number
+    return_value = first_number + second_number 
     return return_value
